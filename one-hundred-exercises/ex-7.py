a8b4f869cb3ebd8fa2f533423f4ba49657f38c61
ngocdung <dungdidom@gmail.com>
2022-01-09 18:29:19 +0900

    ex-7: greatest common divisor

---
+++ one-hundred-exercises/ex-7.py
@@ -0,0 +1,24 @@
+# determines the greatest common divisor of two numbers
+def greatest_common_divisor(n1, n2):
+    factor_n1 = []
+    factor_n2 = []
+    common_divisor = []
+    for i in range(1, n1+1):
+        if n1 % i == 0:
+            factor_n1.append(i)
+    for i in range(1, n2+1):
+        if n2 % i == 0:
+            factor_n2.append(i)
+    for i in factor_n1:
+        for j in factor_n2:
+            if i == j:
+                common_divisor.append(i)
+    return max(common_divisor)
+
+# def greatest_common_divisor(a, b):
+#     while b:
+#         a, b = b, a % b
+#     return a
+
+print(greatest_common_divisor(32,100))
+
