65d46024ad458e9dc046e6526302194c91d614c9
ngocdung <dungdidom@gmail.com>
2022-01-13 19:05:39 +0900

    Ex 9

--- one-hundred-exercises/ex-9.py
+++ one-hundred-exercises/ex-9.py
@@ -1,14 +1,37 @@
-# returns a prime number at position 100
+# Ex9:prime number at position n
 def is_prime(n):
-    if n <=1:
+   
+    if n < 2:
         return False
-    else:
-        for i in range(2, n): 
-            if n % i == 0:
-                return False
+       
+    if n % 2 == 0:
+        return n == 2
+       
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+   
     return True
-
+   
 def calculate(n):
-    prime_list = []
-    while i < n:
-        
+    list = []
+    element = 2
+    while len(list) < n:
+        if is_prime(element):
+            list.append(element)
+        element += 1
+    return list[-1]
+
+print(calculate(100))
+
+# def calculate():
+#     counter = 0
+#     number = 2
+#     while True:
+#         if is_prime(number):
+#             counter += 1
+#             if counter == 100:
+#                 return number
+#         number += 1
