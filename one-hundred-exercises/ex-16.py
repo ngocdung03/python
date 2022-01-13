3b8830cdc57c6d1223f3a56286864d0080e58ad0
ngocdung <dungdidom@gmail.com>
2022-01-13 19:10:41 +0900

    Ex 16

---
+++ one-hundred-exercises/ex-16.py
@@ -0,0 +1,14 @@
+# Ex 16: input matrix as string and return as nested lists
+
+# class Matrix(m):
+   
+#     __init__(m):
+       
+       
+       
+def Matrix(m):
+    result = [row for row in m.split("\n")]
+    result2 =
+    return result
+   
+print(Matrix('3 4\n5 6'))
